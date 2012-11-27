2fcbeffae18c2c03fa33cb3bdd9f9987de196d1a
dmapopov <dmapopov@localhost>
2012-11-27 11:06:12 +0800

    Correcting errors when using the STR ​​length over 128 bytes. Algorithm is copied from the original Connector/C.

--- src/main/java/org/tarantool/core/proto/Leb128.java
+++ src/main/java/org/tarantool/core/proto/Leb128.java
@@ -4,79 +4,100 @@ import java.nio.ByteBuffer;
 
 /**
  * Leb128 encoder/decoder
+ *
  * 
- * @author dgreen
- * @version $Id: $
  */
 public class Leb128 {
 
-	/**
-	 * <p>
-	 * readUnsigned.
-	 * </p>
-	 * 
-	 * @param buffer
-	 *            a {@link java.nio.ByteBuffer} object.
-	 * @return a int.
-	 */
-	public static int readUnsigned(ByteBuffer buffer) {
-		int result = 0;
-		int cur;
-		int count = 0;
+    /**
+     * <p> readUnsigned. </p>
+     *
+     * @param buffer a {@link java.nio.ByteBuffer} object.
+     * @return a int.
+     */
+    public static int readUnsigned(ByteBuffer buffer) {
 
-		do {
-			cur = buffer.get() & 0xff;
-			result |= (cur & 0x7f) << (count * 7);
-			count++;
-		} while (((cur & 0x80) == 0x80) && count < 5);
+        byte b0 = buffer.get();
+        if ((b0 & 0x80) != 0x80) {
+            return b0 & 0x7f;
+        }
 
-		if ((cur & 0x80) == 0x80) {
-			throw new IllegalArgumentException("Can't read LEB128 from buffer");
-		}
+        byte b1 = buffer.get();
+        if ((b1 & 0x80) != 0x80) {
+            return (b0 & 0x7f) << 7
+                    | (b1 & 0x7f);
+        }
+        byte b2 = buffer.get();
+        if ((b2 & 0x80) != 0x80) {
+            return (b0 & 0x7f) << 14
+                    | (b1 & 0x7f) << 7
+                    | (b2 & 0x7f);
+        }
+        byte b3 = buffer.get();
+        if ((b3 & 0x80) != 0x80) {
+            return (b0 & 0x7f) << 21
+                    | (b1 & 0x7f) << 14
+                    | (b2 & 0x7f) << 7
+                    | (b3 & 0x7f);
+        }
+        byte b4 = buffer.get();
+        if ((b4 & 0x80) != 0x80) {
+            return (b0 & 0x7f) << 28
+                    | (b1 & 0x7f) << 21
+                    | (b2 & 0x7f) << 14
+                    | (b3 & 0x7f) << 7
+                    | (b4 & 0x7f);
+        }
 
-		return result;
-	}
+        throw new IllegalArgumentException("Can't read LEB128 from buffer");
 
-	/**
-	 * <p>
-	 * unsignedSize.
-	 * </p>
-	 * 
-	 * @param value
-	 *            a int.
-	 * @return a int.
-	 */
-	public static int unsignedSize(int value) {
-		int remaining = value >> 7;
-		int count = 0;
-		while (remaining != 0) {
-			remaining >>= 7;
-			count++;
-		}
-		return count + 1;
-	}
+    }
 
-	/**
-	 * <p>
-	 * writeUnsigned.
-	 * </p>
-	 * 
-	 * @param buffer
-	 *            a {@link java.nio.ByteBuffer} object.
-	 * @param value
-	 *            a int.
-	 * @return a {@link java.nio.ByteBuffer} object.
-	 */
-	public static ByteBuffer writeUnsigned(ByteBuffer buffer, int value) {
-		int remaining = value >>> 7;
+    /**
+     * <p> unsignedSize. </p>
+     *
+     * @param value a int.
+     * @return a int.
+     */
+    public static int unsignedSize(int value) {
 
-		while (remaining != 0) {
-			buffer.put((byte) ((value & 0x7f) | 0x80));
-			value = remaining;
-			remaining >>>= 7;
-		}
+        if (value < (1 << 7)) {
+            return 1;
+        }
+        if (value < (1 << 14)) {
+            return 2;
+        }
+        if (value < (1 << 21)) {
+            return 3;
+        }
+        if (value < (1 << 28)) {
+            return 4;
+        }
+        return 5;
+    }
 
-		buffer.put((byte) (value & 0x7f));
-		return buffer;
-	}
+    /**
+     * <p> writeUnsigned. </p>
+     *
+     * @param buffer a {@link java.nio.ByteBuffer} object.
+     * @param value a int.
+     * @return a {@link java.nio.ByteBuffer} object.
+     */
+    public static ByteBuffer writeUnsigned(ByteBuffer buffer, int value) {
+
+        if (value >= (1 << 7)) {
+            if (value >= (1 << 14)) {
+                if (value >= (1 << 21)) {
+                    if (value >= (1 << 28)) {
+                        buffer.put((byte) ((value >> 28) | 0x80));
+                    }
+                    buffer.put((byte) ((value >> 21) | 0x80));
+                }
+                buffer.put((byte) ((value >> 14) | 0x80));
+            }
+            buffer.put((byte) ((value >> 7) | 0x80));
+        }
+        buffer.put((byte) ((value) & 0x7F));
+        return buffer;
+    }
 }
